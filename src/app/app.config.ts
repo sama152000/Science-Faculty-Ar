9b7d4042c3b83fbf48fe4034241cbc86eda85003
sama152000 <sama.sama152000@gmail.com>
2025-11-27 14:19:09 +0200

    initlization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { importProvidersFrom } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(ButtonModule)
   ]
 };
